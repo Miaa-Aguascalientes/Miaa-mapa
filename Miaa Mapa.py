2ebfe463e8f089f20f49433aa962a46a9d3ef494
Miaa <pedroguillermomiaa@gmail.com>
2026-08-26 14:23:32 -0600

    Update print statement from 'Hello' to 'Goodbye'

--- Miaa Mapa.py
+++ Miaa Mapa.py
@@ -609,5 +609,5 @@ if ver_pozos:
 
     fg_pozos.add_to(m)
 
-# Renderizar el mapa en Streamlit
-st_folium(m, width="100%", height=700)
+# Renderizar el mapa en Streamlit sin recargas innecesarias
+st_folium(m, width="100%", height=700, returned_objects=[])
